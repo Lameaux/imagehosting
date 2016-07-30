3d5a46b9e02715dc45170b863ae137693df9755b
ssizov <sergii.sizov@showmax.com>
2016-07-31 00:03:49 +0200

    upload to public folder

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,6 +1,7 @@
 // http://stackoverflow.com/questions/4459379/preview-an-image-before-it-is-uploaded
 
 var upload_files = [];
+var upload_results = [];
 
 !function ($) {
   $(function () {
@@ -23,6 +24,21 @@ var upload_files = [];
       }
       printTemplates();
     });
+
+    $("#upload_button").click(function(){
+      $('.preview-remove').addClass('hidden');
+      $('.thumb_status').removeClass('hidden');
+
+      $("#upload_files_label").addClass('hidden');
+      $(this).addClass('hidden');
+
+      for (var i = 0; i < upload_files.length; i++) {
+        upload_results[i] = false;
+      }
+      continueUploading();
+    });
+
+
   });
 }(jQuery);
 
@@ -35,6 +51,16 @@ var removeThumbnail = function() {
   printTemplates();
 };
 
+var selectText = function() {
+  this.setSelectionRange(0, this.value.length);
+};
+
+var copyToClipboard = function() {
+  var image_url = ($(this).parents('.thumb_status').find('.image-url'))[0];
+  image_url.setSelectionRange(0, image_url.value.length);
+  document.execCommand("copy");
+};
+
 function createThumbnail(file, index) {
 
   var thumbnail_html = '<div class="col-md-3 col-sm-4 col-xs-6" id="preview_' + index + '">' +
@@ -42,8 +68,9 @@ function createThumbnail(file, index) {
       '<img class="thumb_cover" alt="' + file.name + '" title="' + file.name + ' (' + file.size + ' bytes)" src="/img/1px.gif">' +
       '</div>' +
       '<p class="thumb_detail">' +
-      '<button class="btn btn-default btn-xs preview-remove" title="Remove" data-id="' + index + '"><span class="glyphicon glyphicon-remove"></span></button> ' + file.name +
+      '<button class="btn btn-default btn-xs preview-remove" title="Remove" data-id="' + index + '"><span class="glyphicon glyphicon-trash"></span></button> ' + file.name +
       '</p>' +
+      '<p class="thumb_status hidden">Pending...</p>' +
       '</div>';
   var thumbnail = $(thumbnail_html);
   thumbnail.find('.preview-remove').click(removeThumbnail);
@@ -64,5 +91,56 @@ function printTemplates() {
   for (var i=0; i < upload_files.length; i++) {
     var file = upload_files[i];
     $('#preview_files').append(createThumbnail(file, i));
+  }
+}
+
+function uploadFile(id) {
+
+  $('#preview_' + id).find('.thumb_status').html('Uploading...');
+
+  var formData = new FormData();
+  formData.append('file', upload_files[id]);
+
+  $.ajax({
+    url : '/upload',
+    type : 'POST',
+    data : formData,
+    processData: false,  // tell jQuery not to process the data
+    contentType: false,  // tell jQuery not to set contentType
+    success : function(data) {
+
+      upload_results[id] = data;
+
+      var success_html = $('<div class="input-group">' +
+      '<span class="input-group-addon">Link</span>' +
+      '<input type="text" class="form-control image-url" aria-label="Link" value="' + data.url + '" readonly>' +
+      '<span class="input-group-btn">' +
+          '<button class="btn btn-default clipboard-btn" title="Copy to Clipboard">' +
+            '<span class="glyphicon glyphicon-copy"></span>' +
+          '</button>' +
+      '</span>' +
+      '</div>');
+
+      success_html.find('.image-url').click(selectText);
+      success_html.find('.clipboard-btn').click(copyToClipboard);
+
+      $('#preview_' + id).find('.thumb_status').html(success_html);
+      continueUploading();
+    },
+    error: function() {
+      upload_results[id] = 'failed';
+      $('#preview_' + id).find('.thumb_status').html('Failed... Try again');
+      continueUploading();
+    }
+
+  });
+
+}
+
+function continueUploading() {
+  for (var i = 0; i < upload_results.length; i++) {
+    if (upload_results[i] == false) {
+      return uploadFile(i);
+    }
   }
 }
--- app/controllers/upload_controller.rb
+++ app/controllers/upload_controller.rb
@@ -1,6 +1,30 @@
+require 'securerandom'
+require 'shortuuid'
 class UploadController < ApplicationController
 
   def create
-    render plain: 'ok'
+
+    uploaded_io = params[:file]
+
+    id = ShortUUID.shorten(SecureRandom.uuid)
+    url = "http://0.0.0.0:3000/uploads/#{id}"
+    content_type = uploaded_io.content_type
+    file_name = uploaded_io.original_filename
+
+    local_file_name = Rails.root.join('public', 'uploads', id)
+    File.open(local_file_name, 'wb') do |file|
+      file.write(uploaded_io.read)
+    end
+
+    file_size = File.size(local_file_name)
+
+    file_info = {
+      id: id,
+      url: url,
+      content_type: content_type,
+      file_name: file_name,
+      file_size: file_size,
+    }
+    render json: file_info
   end
 end
