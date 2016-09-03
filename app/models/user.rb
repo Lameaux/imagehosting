51f77d5a55a147bd10df4fbb7b8baa69ad58dac7
ssizov <sergii.sizov@showmax.com>
2016-09-03 17:43:06 +0200

    password validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,7 +11,7 @@ class User < ApplicationRecord
             uniqueness: {case_sensitive: false, message: 'is already used'},
             format: {with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, message: 'is invalid'}
 
-  validates :password, on: :create,
+  validates :password, if: lambda { |m| m.password.present? },
             length: { minimum: 6, maximum: 255, too_short: 'must be at least 6 characters', too_long: 'must not be longer than 255' }
 
   has_secure_password
