264601fbb26ae766fbe50ed3905ac13d5c199913
ssizov <sergii.sizov@showmax.com>
2016-09-03 11:26:48 +0200

    password min length

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,6 +11,8 @@ class User < ApplicationRecord
             uniqueness: {case_sensitive: false, message: 'is already used'},
             format: {with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, message: 'is invalid'}
 
+  validates :password,
+            length: { minimum: 6, maximum: 255, too_short: 'must be at least 6 characters', too_long: 'must not be longer than 255' }
 
   has_secure_password
 
